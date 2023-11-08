68d56a224ca2eda8c81b3e01517d37c3cbdb0222
pedroanicio <pedroarthurcunha17@gmail.com>
2023-11-07 23:07:38 -0300

    commit 2

--- src/filosofosFamintos.java
+++ src/filosofosFamintos.java
@@ -16,8 +16,9 @@ public class filosofosFamintos {
                 while(true){
                     //filósofo está pensando
                     acao(": Pensando");
-                    synchronized (garfoEsquerdo){ // apenas um filósofo pode pegar o garfo à esquerda. Caso outro tente pegá-lo, ele aguardará até que o seja liberado
+                    synchronized (garfoEsquerdo) { // apenas um filósofo pode pegar o garfo à esquerda. Caso outro tente pegá-lo, ele aguardará até que o seja liberado
                         acao(": Pegou o garfo esquerdo");
+
                         synchronized (garfoDireito){//garante que apenas um filósofo por vez possa pegar o garfo à direita.
                             // Isso evita deadlock, onde todos os filósofos pegariam um garfo à esquerda e esperariam indefinidamente por um garfo à direita.
 
@@ -37,14 +38,14 @@ public class filosofosFamintos {
         }
         //metodo para filósfo realizar uma ação(comer, pensar, pegar os garfos)
         private void acao(String acao) throws InterruptedException{
-            System.out.println(Thread.currentThread().getName() + "" + acao);
+            System.out.println(Thread.currentThread().getName() + acao);
             Thread.sleep(((int) (Math.random() * 100)));
         }
 
     }
 
     public static void main(String[] args) throws Exception{
-        int qtde = 5; //número de filósofos
+        int qtde = 2; //número de filósofos
         Filosofo[] filosofos = new Filosofo[qtde];
 
         Object[] garfos = new Object[filosofos.length]; // número de garfos é o mesmo que o número de filósofos
@@ -58,7 +59,12 @@ public class filosofosFamintos {
             Object garfoEsquerdo = garfos[i];
             Object garfoDireito = garfos[(i + 1) % garfos.length];
 
-            filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito);
+            if(i == filosofos.length - 1){
+                filosofos[i] = new Filosofo(garfoDireito, garfoEsquerdo); // último filósofo pega o garfo da direita primeiro para evitar deadlock
+            }else {
+                filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito);
+            }
+
             Thread t = new Thread(filosofos[i], "Filosofo " + (i + 1));
             t.start(); // Cria threads para os filósofos e inicia sua execução
         }
